364bccee44b83ef1d992067d62e006a982ffa7f8
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-06-08 13:52:57 -0500

    routes

--- 301finalfrontend/src/Componets/Header.jsx
+++ 301finalfrontend/src/Componets/Header.jsx
@@ -1,9 +1,22 @@
+import { AppBar, Button,ButtonGroup  } from "@mui/material";
+import { Link } from "react-router-dom";
+
 function Header() {
-    return (  
-        <>
-        
-        </>
-    );
+  return (
+      <AppBar>
+        <ButtonGroup>
+        <Button variant="text">
+          <Link to="/" className="nav-link">Home</Link>
+        </Button>
+        <Button>
+          <Link to="/tournament-bracket" className="nav-link">Tournament Bracket</Link>
+        </Button>
+        <Button>
+          <Link to="/profile" className="nav-link">Profile</Link>
+        </Button>
+        </ButtonGroup>
+      </AppBar>
+  );
 }
 
 export default Header;
