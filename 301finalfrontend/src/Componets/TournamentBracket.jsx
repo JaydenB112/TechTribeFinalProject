a38b9d59a5f49ebe036d068f8fc008d4d342bed8
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-06-09 21:46:57 -0500

    Update TournamentBracket.jsx

--- 301finalfrontend/src/Componets/TournamentBracket.jsx
+++ 301finalfrontend/src/Componets/TournamentBracket.jsx
@@ -1,9 +1,18 @@
+import { Button, ButtonGroup } from "@mui/material";
+import { Bracket, RoundProps } from 'react-brackets';
+
+
 function TournamentBracket() {
-    return ( 
-        <div>
-            <p>Tournament</p>
-        </div>
-     );
+  return (
+    <div>
+      <p>Tournament</p>
+      <ButtonGroup variant="contained">
+        <Button color="success"> make tournament</Button>
+        <Button>remake tournament</Button>
+        <Button color="error">clear tournament</Button>
+      </ButtonGroup>
+    </div>
+  );
 }
 
 export default TournamentBracket;
