5b830c955334cb33aa94910c3b45decf11c5f118
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-06-11 00:56:58 -0500

    Update TournamentForm.jsx

--- 301finalfrontend/src/Componets/TournamentForm.jsx
+++ 301finalfrontend/src/Componets/TournamentForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, Button, FormControl,FormGroup,FormControlLabel,TextField,Switch  } from "@mui/material";
+import { Modal, Button, FormControl,FormControlLabel,TextField,Switch  } from "@mui/material";
 
 function TournamentForm() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -21,11 +21,18 @@ function TournamentForm() {
         <Button color="success" onClick={handleOpenModal}> make tournament</Button>
       <Modal open={isModalOpen} onClose={handleCloseModal}>
         <form className="form-container"  onSubmit={handleSubmit}>
-        {/* <FormControl>
-        <FormControlLabel control={<Switch defaultChecked />} label="double elimination" />
+        <FormControl>
+            <h2>Tournament Form</h2>
+            <br/>
             <TextField id="outlined-basic" label="competitor name" variant="outlined" />
-        </FormControl> */}
+            <ul>
+                <li></li>
+            </ul>
+            <br/>
+            <FormControlLabel control={<Switch/>} label="double elimination" />
+            <br/>
             <Button type="submit">Submit</Button>
+        </FormControl>
           </form>
       </Modal>
     </>
