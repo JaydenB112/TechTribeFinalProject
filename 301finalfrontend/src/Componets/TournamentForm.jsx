bfb88df8f79c357fef52cc4b784db63caee9633b
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-06-11 00:50:37 -0500

    Update TournamentForm.jsx

--- 301finalfrontend/src/Componets/TournamentForm.jsx
+++ 301finalfrontend/src/Componets/TournamentForm.jsx
@@ -1,19 +1,35 @@
-import { Modal,Button, ButtonGroup } from "@mui/material";
-
+import React, { useState } from "react";
+import { Modal, Button, FormControl,FormGroup,FormControlLabel,TextField,Switch  } from "@mui/material";
 
 function TournamentForm() {
-    return ( 
-        <>
-        <Modal>
-            <Button>submit</Button>
-        </Modal>
-        
-        <Modal>
-            <Button>yes</Button>
-            <Button>no</Button>
-        </Modal>
-        </>
-     );
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSubmit = () => {
+    alert("Form submitted!");
+  };
+  
+  return (
+    <>
+        <Button color="success" onClick={handleOpenModal}> make tournament</Button>
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+        <form className="form-container"  onSubmit={handleSubmit}>
+        {/* <FormControl>
+        <FormControlLabel control={<Switch defaultChecked />} label="double elimination" />
+            <TextField id="outlined-basic" label="competitor name" variant="outlined" />
+        </FormControl> */}
+            <Button type="submit">Submit</Button>
+          </form>
+      </Modal>
+    </>
+  );
 }
 
 export default TournamentForm;
