3869073ed989f81b29063ad590d2eda8003f1df6
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-06-13 12:08:24 -0500

    Update CompetitorPosition.jsx

--- 301finalfrontend/src/Componets/Brack/CompetitorPosition.jsx
+++ 301finalfrontend/src/Componets/Brack/CompetitorPosition.jsx
@@ -1,7 +1,15 @@
-function CompetitorPosition() {
+import {
+    Button
+  } from "@mui/material";
+  
+
+function CompetitorPosition(tournamentList) {
     return ( 
         <>
-        
+        <Button>+</Button>
+        <p>ss</p>
+        <Button>-</Button>
+        <p>1</p>
         </>
      );
 }
