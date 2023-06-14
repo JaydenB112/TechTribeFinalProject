3743f9a1c2e1fc19c44258df2b4b5e044840c91f
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-06-14 13:19:52 -0500

    TTFrontend

--- 301finalfrontend/src/Componets/Home.jsx
+++ 301finalfrontend/src/Componets/Home.jsx
@@ -1,9 +1,29 @@
+import React, { useEffect } from "react";
+import { Button } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Home() {
-    return (  
-        <div>
-            <p>Welcome</p>
-        </div>
-    );
+    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    // Add any necessary initialization logic here
+  }, []);
+
+  return (
+    <div className="welcome-container">
+      <header>
+        <h1>Welcome</h1>
+        {!isAuthenticated && (
+          <Button onClick={() => loginWithRedirect()}>Log In</Button>
+        )}
+        {isAuthenticated && (
+          <Button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Button>
+        )}
+      </header>
+    </div>
+  );
 }
+    
 
 export default Home;
